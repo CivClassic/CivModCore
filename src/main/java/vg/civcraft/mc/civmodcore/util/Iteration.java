e228514136ff1b9aaccb098c11216fe12f66431d
Alexander <protonull@protonmail.com>
2020-09-04 08:22:22 +0100

    Iteration :: addAll API

--- src/main/java/vg/civcraft/mc/civmodcore/util/Iteration.java
+++ src/main/java/vg/civcraft/mc/civmodcore/util/Iteration.java
@@ -22,7 +22,7 @@ public final class Iteration {
 	 * @deprecated Use {@link ArrayUtils#isEmpty(Object[])} instead.
 	 */
 	@Deprecated
-	public static <T> boolean isNullOrEmpty(T[] array) {
+	public static <T> boolean isNullOrEmpty(final T[] array) {
 		return ArrayUtils.isEmpty(array);
 	}
 
@@ -36,7 +36,7 @@ public final class Iteration {
 	 * @param collection The collection to check.
 	 * @return Returns true if the collection exists and at least one item.
 	 */
-	public static <T> boolean isNullOrEmpty(Collection<T> collection) {
+	public static <T> boolean isNullOrEmpty(final Collection<T> collection) {
 		return collection == null || collection.isEmpty();
 	}
 
@@ -47,7 +47,7 @@ public final class Iteration {
 	 * @param array The array to retrieve the length of.
 	 * @return Returns the length of the array, or 0 if null.
 	 */
-	public static <T> int arrayLength(T[] array) {
+	public static <T> int arrayLength(final T[] array) {
 		return array == null ? 0 : array.length;
 	}
 
@@ -58,7 +58,7 @@ public final class Iteration {
 	 * @param collection The collection to retrieve the size of.
 	 * @return Returns the size of the collection, or 0 if null.
 	 */
-	public static <T> int collectionSize(Collection<T> collection) {
+	public static <T> int collectionSize(final Collection<T> collection) {
 		return collection == null ? 0 : collection.size();
 	}
 
@@ -72,11 +72,11 @@ public final class Iteration {
 	 * @return Returns true if the base is found within the values.
 	 */
 	@SafeVarargs
-	public static <T> boolean contains(T base, T... values) {
+	public static <T> boolean contains(final T base, final T... values) {
 		if (isNullOrEmpty(values)) {
 			return false;
 		}
-		for (T value : values) {
+		for (final T value : values) {
 			if (Objects.equals(base, value)) {
 				return true;
 			}
@@ -91,11 +91,11 @@ public final class Iteration {
 	 * @param collection The collection to iterate and clear.
 	 * @param processor The iteration processor which will be called for each item in the collection.
 	 */
-	public static <T> void iterateThenClear(Collection<T> collection, Consumer<T> processor) {
+	public static <T> void iterateThenClear(final Collection<T> collection, final Consumer<T> processor) {
 		if (isNullOrEmpty(collection) || processor == null) {
 			return;
 		}
-		for (T element : collection) {
+		for (final T element : collection) {
 			processor.accept(element);
 		}
 		collection.clear();
@@ -108,11 +108,11 @@ public final class Iteration {
 	 * @param collection The collection to iterate.
 	 * @param processor The iteration processor which will be called for each item in the collection.
 	 */
-	public static <T> void iterateHasNext(Collection<T> collection, ElementAndBoolConsumer<T> processor) {
+	public static <T> void iterateHasNext(final Collection<T> collection, final ElementAndBoolConsumer<T> processor) {
 		if (isNullOrEmpty(collection) || processor == null) {
 			return;
 		}
-		Iterator<T> iterator = collection.iterator();
+		final Iterator<T> iterator = collection.iterator();
 		while (iterator.hasNext()) {
 			processor.accept(iterator.next(), iterator.hasNext());
 		}
@@ -126,7 +126,7 @@ public final class Iteration {
 	 * @param value The value to fill the array with.
 	 * @return Returns the given array with the filled values.
 	 */
-	public static <T> T[] fill(T[] array, T value) {
+	public static <T> T[] fill(final T[] array, final T value) {
 		if (isNullOrEmpty(array)) {
 			return array;
 		}
@@ -136,7 +136,9 @@ public final class Iteration {
 
 	/** @deprecated Use {@link #anyMatch(Object[], Predicate)} instead. */
 	@Deprecated
-	public static <T> boolean some(T[] array, Predicate<T> predicate) { return anyMatch(array, predicate); }
+	public static <T> boolean some(final T[] array, final Predicate<T> predicate) {
+		return anyMatch(array, predicate);
+	}
 
 	/**
 	 * <p>Tests whether there is at least one element in the given array that passes the criteria of the given
@@ -150,14 +152,14 @@ public final class Iteration {
 	 * @return Returns true if at least one element passes the predicate test. Or false if the array fails the
 	 * {@link Iteration#isNullOrEmpty(Object[]) isNullOrEmpty()} test, or true if the give predicate is null.
 	 */
-	public static <T> boolean anyMatch(T[] array, Predicate<T> predicate) {
+	public static <T> boolean anyMatch(final T[] array, final Predicate<T> predicate) {
 		if (isNullOrEmpty(array)) {
 			return false;
 		}
 		if (predicate == null) {
 			return true;
 		}
-		for (T element : array) {
+		for (final T element : array) {
 			if (predicate.test(element)) {
 				return true;
 			}
@@ -167,7 +169,9 @@ public final class Iteration {
 
 	/** @deprecated Use {@link #allMatch(Object[], Predicate)} instead. */
 	@Deprecated
-	public static <T> boolean every(T[] array, Predicate<T> predicate) { return allMatch(array, predicate); }
+	public static <T> boolean every(final T[] array, final Predicate<T> predicate) {
+		return allMatch(array, predicate);
+	}
 
 	/**
 	 * <p>Tests whether every element in an array passes the criteria of the given predicate.</p>
@@ -180,14 +184,14 @@ public final class Iteration {
 	 * @return Returns true if no element fails the predicate test, or if the array fails the
 	 * {@link Iteration#isNullOrEmpty(Object[]) isNullOrEmpty()} test, or if the give predicate is null.
 	 */
-	public static <T> boolean allMatch(T[] array, Predicate<T> predicate) {
+	public static <T> boolean allMatch(final T[] array, final Predicate<T> predicate) {
 		if (isNullOrEmpty(array)) {
 			return true;
 		}
 		if (predicate == null) {
 			return true;
 		}
-		for (T element : array) {
+		for (final T element : array) {
 			if (!predicate.test(element)) {
 				return false;
 			}
@@ -201,7 +205,7 @@ public final class Iteration {
 	 * @param entry The map entry itself.
 	 * @return Returns true if the entry is considered valid.
 	 */
-	public static boolean validEntry(Map.Entry<?, ?> entry) {
+	public static boolean validEntry(final Map.Entry<?, ?> entry) {
 		if (entry == null) {
 			return false;
 		}
@@ -224,23 +228,40 @@ public final class Iteration {
 	 * collection.
 	 */
 	@SafeVarargs
-	public static <T, K extends Collection<T>> K collect(Supplier<K> constructor, T... elements) {
+	public static <T, K extends Collection<T>> K collect(final Supplier<K> constructor, final T... elements) {
 		if (constructor == null) {
 			return null;
 		}
-		K collection = constructor.get();
+		final K collection = constructor.get();
 		if (collection == null) {
 			return null;
 		}
 		if (isNullOrEmpty(elements)) {
 			return collection;
 		}
-		for (T element : elements) {
+		addAll(collection, elements);
+		return collection;
+	}
+
+	/**
+	 * Use this to add arbitrary amounts of things to a collection that doesn't already exist as a collection,
+	 * which means you can avoid the dreaded {@code Arrays.asList(thing, thing2);}, thereby creating a new list
+	 * just to immediately discard it in the next operation execution.
+	 *
+	 * @param <T> The type of the elements of the collection and to add.
+	 * @param collection The collection to add the values to.
+	 * @param values The values to add to the collection.
+	 */
+	@SafeVarargs
+	public static <T> void addAll(final Collection<T> collection, final T... values) {
+		if (collection == null || ArrayUtils.isEmpty(values)) {
+			return;
+		}
+		for (final T value : values) {
 			// Do not let this be simplified. There's no reason to create a new ArrayList
 			// as it would be immediately discarded and that's... bad
-			collection.add(element);
+			collection.add(value);
 		}
-		return collection;
 	}
 
 	/**
@@ -263,7 +284,7 @@ public final class Iteration {
 	 *
 	 * @param list The list to remove the last element from.
 	 */
-	public static <T> T removeLastElement(List<T> list) {
+	public static <T> T removeLastElement(final List<T> list) {
 		if (isNullOrEmpty(list)) {
 			return null;
 		}
