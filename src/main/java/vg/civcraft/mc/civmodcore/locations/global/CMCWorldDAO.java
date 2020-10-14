f1195f5ca16981c72f7c77edf48b508d073e2127
psygate <psygate@users.noreply.github.com>
2020-10-14 20:23:01 +0200

    Fix the dependency issue where you can't drop a primary key constraint on a table where foreign key constraints exist.

--- src/main/java/vg/civcraft/mc/civmodcore/locations/global/CMCWorldDAO.java
+++ src/main/java/vg/civcraft/mc/civmodcore/locations/global/CMCWorldDAO.java
@@ -1,20 +1,12 @@
 package vg.civcraft.mc.civmodcore.locations.global;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.logging.Logger;
-
-import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import vg.civcraft.mc.civmodcore.CivModCorePlugin;
 import vg.civcraft.mc.civmodcore.dao.ManagedDatasource;
+
+import java.sql.*;
+import java.util.logging.Logger;
 
 public class CMCWorldDAO {
 
@@ -28,8 +20,8 @@ public class CMCWorldDAO {
 
 	public short getOrCreatePluginID(String identifier) {
 		try (Connection insertConn = db.getConnection();
-				PreparedStatement insertPlugin = insertConn
-						.prepareStatement("select id from cmc_plugins where name = ?;")) {
+			 PreparedStatement insertPlugin = insertConn
+					 .prepareStatement("select id from cmc_plugins where name = ?;")) {
 			insertPlugin.setString(1, identifier);
 			try (ResultSet rs = insertPlugin.executeQuery()) {
 				if (rs.next()) {
@@ -41,8 +33,8 @@ public class CMCWorldDAO {
 			return -1;
 		}
 		try (Connection insertConn = db.getConnection();
-				PreparedStatement insertPlugin = insertConn.prepareStatement(
-						"insert into cmc_plugins (name) values(?);", Statement.RETURN_GENERATED_KEYS);) {
+			 PreparedStatement insertPlugin = insertConn.prepareStatement(
+					 "insert into cmc_plugins (name) values(?);", Statement.RETURN_GENERATED_KEYS);) {
 			insertPlugin.setString(1, identifier);
 			insertPlugin.execute();
 			try (ResultSet rs = insertPlugin.getGeneratedKeys()) {
@@ -64,8 +56,8 @@ public class CMCWorldDAO {
 
 	short getOrCreateWorldID(World world) {
 		try (Connection insertConn = db.getConnection();
-				PreparedStatement insertWorld = insertConn
-						.prepareStatement("select id from cmc_worlds where uuid = ?;")) {
+			 PreparedStatement insertWorld = insertConn
+					 .prepareStatement("select id from cmc_worlds where uuid = ?;")) {
 			insertWorld.setString(1, world.getUID().toString());
 			try (ResultSet rs = insertWorld.executeQuery()) {
 				if (rs.next()) {
@@ -77,8 +69,8 @@ public class CMCWorldDAO {
 			return -1;
 		}
 		try (Connection insertConn = db.getConnection();
-				PreparedStatement insertWorld = insertConn.prepareStatement(
-						"insert into cmc_worlds (uuid, name) values(?,?);", Statement.RETURN_GENERATED_KEYS);) {
+			 PreparedStatement insertWorld = insertConn.prepareStatement(
+					 "insert into cmc_worlds (uuid, name) values(?,?);", Statement.RETURN_GENERATED_KEYS);) {
 			insertWorld.setString(1, world.getUID().toString());
 			insertWorld.setString(2, world.getName());
 			insertWorld.execute();
@@ -101,17 +93,41 @@ public class CMCWorldDAO {
 						+ "name text not null, constraint uniqueUuid unique(uuid));",
 				"create table if not exists cmc_plugins (id smallint unsigned not null auto_increment primary key, name varchar(255) not null, "
 						+ "constraint uniqueName unique(name));",
-				"create table if not exists cmc_chunk_data (x int not null, z int not null, world_id smallint unsigned not null references cmc_worlds(id), "
+				"create table if not exists cmc_chunk_data (" +
+						"x int not null, z int not null, world_id smallint unsigned not null references cmc_worlds(id), "
 						+ "plugin_id smallint unsigned not null references cmc_plugins(id), data text not null,"
-						+ "primary key cmc_chunk_lookup(world_id, x, z, plugin_id));");
-		db.registerMigration(2, false, 
-				"alter table cmc_chunk_data drop primary key",
+						+ "primary key cmc_chunk_lookup(world_id, x, z, plugin_id)" +
+						");");
+
+		/* 	Patch:
+			Errno: 150 "Foreign key constraint is incorrectly formed")
+			java.sql.SQLException: Error on rename of '.\civclassic\#sql-alter-3b34-134' to '.\civclassic\cmc_chunk_data' (errno: 150 "Foreign key constraint is incorrectly formed")
+
+		 	ALTER TABLE cmc_chunk_data DROP CONSTRAINT cmc_chunk_data_ibfk_1;
+			ALTER TABLE cmc_chunk_data DROP CONSTRAINT cmc_chunk_data_ibfk_2;
+			ALTER TABLE cmc_chunk_data DROP CONSTRAINT PRIMARY KEY;
+
+			ALTER TABLE cmc_chunk_data ADD PRIMARY KEY(world_id, x, z, plugin_id);
+			ALTER TABLE cmc_chunk_data ADD CONSTRAINT cmc_chunk_data_ibfk_1 FOREIGN KEY (world_id) REFERENCES cmc_worlds(id) ;
+			ALTER TABLE cmc_chunk_data ADD CONSTRAINT cmc_chunk_data_ibfk_2 FOREIGN KEY (plugin_id) REFERENCES cmc_plugins(id) ;
+
+		 */
+
+		db.registerMigration(2, true,
+				"ALTER TABLE cmc_chunk_data DROP CONSTRAINT cmc_chunk_data_ibfk_1;",
+				"ALTER TABLE cmc_chunk_data DROP CONSTRAINT cmc_chunk_data_ibfk_2;",
+				"ALTER TABLE cmc_chunk_data DROP CONSTRAINT PRIMARY KEY;",
+
 				"alter table cmc_chunk_data change column x chunk_x int not null",
 				"alter table cmc_chunk_data change column z chunk_z int not null",
 				"alter table cmc_chunk_data add column x_offset tinyint unsigned not null",
 				"alter table cmc_chunk_data add column y smallint not null",
 				"alter table cmc_chunk_data add column z_offset tinyint unsigned not null",
-				"alter table cmc_chunk_data add primary key(world_id, chunk_x, chunk_z, plugin_id, x_offset, y, z_offset)");
+				"alter table cmc_chunk_data add primary key(world_id, chunk_x, chunk_z, plugin_id, x_offset, y, z_offset)",
+
+				"ALTER TABLE cmc_chunk_data ADD CONSTRAINT cmc_chunk_data_ibfk_1 FOREIGN KEY (world_id) REFERENCES cmc_worlds(id);",
+				"ALTER TABLE cmc_chunk_data ADD CONSTRAINT cmc_chunk_data_ibfk_2 FOREIGN KEY (plugin_id) REFERENCES cmc_plugins(id);"
+		);
 	}
 
 	public boolean updateDatabase() {
